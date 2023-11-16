f6b49999b78268646bc374bdaf6b814be237ec6a
Daniel Hernandez Lopez <daniherlop@gmail.com>
2023-11-16 12:48:33 -0500

    My second commit

--- src/PartA/Computer.java
+++ src/PartA/Computer.java
@@ -10,14 +10,49 @@ public class Computer {
 	private double price;
 	
 	public Computer(String brand, String model, long serialnum, double price) {
-		super();
 		this.brand = brand;
 		this.model = model;
 		this.serialnum = serialnum;
 		this.price = price;
 	}
 
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public long getSerialnum() {
+		return serialnum;
+	}
+
+	public void setSerialnum(long serialnum) {
+		this.serialnum = serialnum;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Computer [brand=" + brand + ", model=" + model + ", serialnum=" + serialnum + ", price=" + price + "]";
+	}
+
 	
-	if(brand==c.brand&&model==c.model&&price)
 
 }
